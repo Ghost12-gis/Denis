c7a646fb2478e2002c1beb28d99a694ee0f07ad9
Ghost12-gis <gnezdindenis@gmail.com>
2024-09-13 11:20:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let hour = 24 
+let minut = 60 
+let sec = 60 
+console.log ( `в сутках ${hour*minut*sec} секунд`)
+
+
+let hour2 =24 
+let minut2 =60
+let sec2 = 60
+
+console.log (`в 30 сутках ${hour2*30*minut2*sec2} секунд`)
+
+let hour3 =24 
+let minut3 =60
+let sec3 = 60
+
+console.log (`в `)
