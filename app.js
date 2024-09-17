a2c38f72a08b817cd8941e5dcaf1e78456ac4e75
Ghost12-gis <gnezdindenis@gmail.com>
2024-09-17 10:51:05 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -14,4 +14,77 @@ let hour3 =24
 let minut3 =60
 let sec3 = 60
 
-console.log (`в `)
+console.log (`в году ${hour3*365*minut3*sec3} секунд`)
+
+let hour4 =24 
+let minut4 =60
+
+console.log (`в сутках ${hour4*minut4} минут`)
+
+let hour5 = 24
+let minut5 =60 
+
+console.log (`в году ${hour4*365*minut4} минут`)
+
+let byte = 1024 
+let Kb = 1024 
+
+console.log (`в 1 мегабайте ${byte*Kb} байт`)
+
+let b2 = 1024 
+let Kib = 1024
+let Mb = 1024 
+
+console.log (`в 1 гигабайте ${b2*Kib*Mb} байт`)
+
+let byt = 1024 
+let Kilobyte= 1024
+let Megabyte = 1024 
+
+console.log (`в 10 гигабайтах ${byt*Kilobyte*Megabyte*10} байт`)
+
+let byt2 = 1024 
+let Kibt = 1024
+let Mebt = 1024 
+let GB = 1024 
+
+console.log (`в 1 терабайте  ${byt2*Kidt*Megbt*Gb} байт`)
+
+
+let Kibit = 1024 
+let Megabt = 1024 
+let Gibt =1024 
+
+console.log (`в 1 терабайте  ${kibit*Megsbt*Gigb} килобайт`)
+
+
+
+
+
+let r = 6; 
+const pi = 3.14 
+let s = (Math.PI * r ** 2).toFixed (2); 
+console.log(`Площадь круга при радиусе ${r} равна ${s}`)
+
+let a = 6;  
+s = a ** 2;
+console.log(`Площадь квадрата со стороной ${a} равна ${s}`)
+
+let b = 9 
+s = a * b 
+console.log(`Площадь прямоугольника со стронами ${a} и {b} равна ${a} равна {s}`)
+
+
+
+
+let num = 56728 
+let str =String(num)
+console. log(num % 10)
+
+
+let arr = ['a', 'b', 'c', 'd']; 
+console.log(arr[0]+$arr[1] )  
+
+
+let arr2 = ['a', 'b', 'c', 'd', 'e'];
+delete arr[1];
